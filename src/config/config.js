4b5074dbb3e5aa13f8f1c5f7fee8ff830a6d0d48
maolink <maolink@yahoo.com>
2022-11-21 20:06:49 -0500

    cositas

--- src/config/config.js
+++ src/config/config.js
@@ -45,8 +45,8 @@ renderButtonRegister:false, // renderiza el boton de registro
 renderBetRoundGroup: false, // permite que el usuario pueda usar la zona de apuestas para la ronda de grupos
 renderViewOtherBetGroup:true, // Activa el boton que permita ver las apuestas de los otros jugadores
 
-enableMenuRoundPhases:true, // Activa el Menu para acceder a los juegos de las fases
-renderBetRoundPhases:true, // Permite que el usuario puda usar la zonas de apuesas de la ronda por fases
+enableMenuRoundPhases:false, // Activa el Menu para acceder a los juegos de las fases
+renderBetRoundPhases:false, // Permite que el usuario puda usar la zonas de apuesas de la ronda por fases
 renderViewOtherBetPhases:false, // Activa el boton que permita ver las apuestas de los otros jugadores
 renderViewOtherBetClassFinal:false,// Muestra o no el boton para ver las clasificaciones FINAL
 
